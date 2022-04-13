d00691909d6350dd2c8ecdd03697955b9847c2d6
Thiago Alberto <71457360+Thiago1alberto@users.noreply.github.com>
2022-04-13 12:08:55 -0300

    Delete desconto.py

--- desconto.py
+++
@@ -1,5 +0,0 @@
-preço = float(input("digite o valor do produto"))
-desconto = float(input("digite o valor do desconto em %"))
-calculo = preço - (preço * desconto)/100
-desconto = (preço * desconto)/100
-print("o valor do produto vai custar {} e seu desconto foi de {}" .format(calculo, desconto))
